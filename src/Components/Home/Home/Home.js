8c686096dc1cd2119c1d9501e4c64c457c149ac7
Asadullah246 <asadm2258@gmail.com>
2023-02-05 19:11:26 +0600

    appliances banner added

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import AppliancesBanner from '../appliancesBanner/AppliancesBanner';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
-        <div>
+        <div >
             <h1>This is Home</h1>
 
             <Banner></Banner>
+            <AppliancesBanner></AppliancesBanner>
         </div>
     );
 };
