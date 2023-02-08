5a3bd515dff8e01b734e0548bdb6737f35f1cda2
Asadullah246 <asadm2258@gmail.com>
2023-02-08 10:32:32 +0600

    add to cart updated

--- src/Components/Shared/Navbar/UpperNavbar.js
+++ src/Components/Shared/Navbar/UpperNavbar.js
@@ -1,9 +1,80 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../../images/logo.png";
 import { RxCross1 } from "react-icons/rx";
+import cartImg from "../../../imagess/home-product7.png";
+import { coolGray } from "tailwindcss/colors";
 
 const UpperNavbar = () => {
   const [showCart, setShowCart] = useState(false);
+  const [cart, setCart] = useState([
+    {
+      id: 1,
+      name: "Capable countertop microwave oven",
+      image: cartImg,
+      price: 100,
+    },
+    {
+      id: 2,
+      name: "Capable oven",
+      image: cartImg,
+      price: 200,
+    },
+  ]);
+  const [amounts, setAmounts] = useState([
+    {
+      id: 1,
+      amount: 1,
+      price: 100,
+    },
+    {
+      id: 2,
+      amount: 1,
+      price: 200,
+    },
+  ]);
+  const [Subtotal, setSubtotal] = useState(0);
+
+  useEffect(() => {
+    let totalAmount = 0; 
+    for(let i=0; i<amounts.length; i++){
+      totalAmount = totalAmount + (amounts[i].amount * amounts[i].price);
+    }  
+    setSubtotal(totalAmount)
+   
+  }, [amounts]);
+
+  const amountIncrease = (id) => {
+    const others = amounts.filter((p) => p.id != id);
+    const product = amounts.find((p) => p.id == id);
+    product.amount = product.amount + 1;
+    others.push(product);
+    setAmounts(others);
+
+  };
+  const amountDecrease = (id) => {
+    const others = amounts.filter((p) => p.id != id);
+    const product = amounts.find((p) => p.id == id);
+    if (product.amount > 0) {
+      product.amount = product.amount - 1;
+    }
+
+    others.push(product);
+    setAmounts(others);
+
+  };
+
+  const removeFromCart=(id)=>{
+    const remainingCart=cart.filter(c=>c.id !=id)
+    const remainingAmounts=amounts.filter(c=>c.id !=id)
+    setCart(remainingCart)
+    setAmounts(remainingAmounts)
+
+  }
+  const removeAllItems=()=>{
+    setCart([])
+    setAmounts([]) 
+
+  }
   return (
     <div>
       {/* add to cart section  */}
@@ -16,12 +87,12 @@ const UpperNavbar = () => {
               }}
               className="w-full h-full absolute bg-black z-[9999] opacity-50"
             ></div>
-            <div className="w-[350px] h-screen absolute top-0 right-0 bg-white z-[99999] py-8 px-6">
+            <div className="w-[400px] h-screen absolute top-0 right-0 bg-white z-[99999] py-8 px-6">
               <div className="h-[80vh] relative">
-                <div className="flex justify-between items-center ">
+                <div className="flex justify-between items-center mb-6">
                   <h4 className="text-2xl font-bold">Cart (5)</h4>
                   <div className=" flex items-center">
-                    <button className="p-0  cursor-pointer hover:text-[#ff00009f] mr-3 border border-slate-800 border-b-2 border-t-0 border-l-0 border-r-0">
+                    <button className="p-0  cursor-pointer hover:text-[#ff00009f] mr-3 border border-slate-800 border-b-2 border-t-0 border-l-0 border-r-0" onClick={removeAllItems}>
                       Clear all
                     </button>
                     <button
@@ -32,22 +103,68 @@ const UpperNavbar = () => {
                     </button>
                   </div>
                 </div>
+
+                {/* Items  */}
+
+                {cart?.map((c) => {
+                  const amount = amounts.find((a) => a.id == c.id).amount;
+                  return (
+                    <div className="flex justify-between items-center gap-4 mb-6">
+                      <img src={cartImg} alt="" className="w-[60px] h-auto" />
+                      <div>
+                        <div className="flex justify-between items-start gap-10">
+                          <div>
+                            <p>{c.name}</p>
+                            <p className="font-semibold mt-1">{c.price}</p>
+                          </div>
+                          <div>
+                            <button
+                              className="cursor-pointer "
+                              onClick={() => removeFromCart(c.id)}
+                            >
+                              <RxCross1 />
+                            </button>
+                          </div>
+                        </div>
+
+                        <div className="flex justify-between items-center gap-10 mt-1">
+                          <div className="flex items-center border ">
+                            <button
+                              className="bg-[#FAFAFA] px-1 mb-1 border-r font-bold text-lg"
+                              onClick={() => amountDecrease(c.id)}
+                            >
+                              -
+                            </button>
+                            <p className="px-3 font-semibold">{amount}</p>
+                            <button
+                              className="bg-[#FAFAFA] px-1 mb-1 border-l  font-bold text-lg"
+                              onClick={() => amountIncrease(c.id)}
+                            >
+                              +
+                            </button>
+                          </div>
+                          <p className="font-semibold">${amount * c.price}</p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
-              <div className="absolute bottom-4 left-0 w-full px-6"> 
+              <div className="absolute bottom-4 left-0 w-full px-6">
                 <div className="w-full border-b-2"></div>
                 <div className="flex justify-between items-center mt-2">
                   <h6 className="uppercase text-base font-semibold">
                     Subtotal :
                   </h6>
                   <h6 className="text-[#F61818] text-base font-semibold">
-                    $88.00
+                    ${Subtotal}
                   </h6>
                 </div>
                 <div className="flex justify-between items-center mt-4">
-                  <button className="uppercase py-3 px-10  border-4 bg-transparent font-bold text-xs text-black">
+                  <button className="uppercase py-3 px-10  border-2 border-black bg-transparent font-bold text-xs text-black">
                     View cart
                   </button>
-                  <button className="uppercase py-3 px-10  border-non3 bg-[#F50000] font-bold text-xs text-white">
+                  <button className="uppercase py-[14px] px-11  border-non3 bg-[#F50000] font-bold text-xs text-white">
                     Checkout
                   </button>
                 </div>
