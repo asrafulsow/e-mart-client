1b3ad5ca63875df2d1a0b6e8eb64d8ab4ccd3a1a
mahlilmahee <mahlilmahee942@gmail.com>
2023-02-24 11:30:27 +0600

    added

--- src/Components/Home/ProductDetail/ProductDetail.js
+++ src/Components/Home/ProductDetail/ProductDetail.js
@@ -4,18 +4,34 @@ import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
     const [products, setProducts] = useState([]);
-
+  const [detail, setDetail] = useState([])
     let {singleProduct}=useParams()
-    console.log(products,' hedar baal')
+   
+
 
   useEffect(() => {
     axios
       .get(`https://emart-xcpi.onrender.com/products`)
       .then((res) => setProducts(res.data));
   }, []);
+
+
+useEffect(()=>{
+    const mahee= products.filter(x => x?._id === singleProduct); 
+    setDetail(mahee)
+    console.log(detail, 'this is from ami ')
+},[singleProduct, products]);
+
+
+
     return (
         <div>
-            <h1> Hello I this is product page {singleProduct} </h1>
+           {detail && detail.map(data=> {
+           return <h1>df {data?._id} {data?.category} </h1>
+           }
+           
+           )}
+            
         </div>
     );
 };
